9234c4194286f3bba6829bcf6b85644622d0a4af
Sourya <souryaadhikary2911@gmail.com>
2025-06-28 00:47:49 +0530

    Simple storage ended

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -4,8 +4,22 @@ pragma solidity ^0.8.19;
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listsimpleStorage;
+    // address[] public listSimpleStorageAddresses;
+
     function createSimpleStorageContract() public{
-        simpleStorage = new SimpleStorage();
+        SimpleStorage _newSimpleStorage = new SimpleStorage();
+        listsimpleStorage.push(_newSimpleStorage);
+    }
+    function sfStore(uint256 _simpleStorageIndex,uint256 _newSimpleStorageNumber)public{
+        // SimpleStorage _mySimpleStorage = SimpleStorage(listSimpleStorageAddresses[_simpleStorageIndex]);
+        //ABI
+        SimpleStorage _simpleStorage = listsimpleStorage[_simpleStorageIndex];
+        _simpleStorage.store(_newSimpleStorageNumber);
+    }
+    function sfGet(uint256 _simpleStorageIndex)public view returns(uint256){
+        SimpleStorage _simpleStorage = listsimpleStorage[_simpleStorageIndex];
+        
+        return(_simpleStorage.retrieve());
     }
 }
